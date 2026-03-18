4d47f4e40ca2f8edc19cc70020b563b2575cf408
ashad0000 <shadabdul223@gmail.com>
2026-03-18 15:52:18 -0500

    Implemented Count and BoundingBox logic

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -17,38 +17,61 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onFill(final Fill f) {
-        return null;
+        return f.getShape().accept(this);
     }
 
     @Override
     public Location onGroup(final Group g) {
+        var shapes = g.getShapes();
+        if (shapes.isEmpty()) {
+            return new Location(0, 0, new Rectangle(0, 0));
+        }
 
-        return null;
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+
+        for (Shape shape : shapes) {
+            Location b = shape.accept(this);
+            Rectangle r = (Rectangle) b.getShape();
+
+            int left = b.getX();
+            int top = b.getY();
+            int right = left + r.getWidth();
+            int bottom = top + r.getHeight();
+
+            if (left < minX) minX = left;
+            if (top < minY) minY = top;
+            if (right > maxX) maxX = right;
+            if (bottom > maxY) maxY = bottom;
+        }
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
     }
 
     @Override
     public Location onLocation(final Location l) {
-
-        return null;
+        Location innerBounds = l.getShape().accept(this);
+        return new Location(l.getX() + innerBounds.getX(), l.getY() + innerBounds.getY(), innerBounds.getShape());
     }
 
     @Override
     public Location onRectangle(final Rectangle r) {
-        return null;
+        return new Location(0, 0, new Rectangle(r.getWidth(), r.getHeight()));
     }
 
     @Override
     public Location onStrokeColor(final StrokeColor c) {
-        return null;
+        return c.getShape().accept(this);
     }
 
     @Override
     public Location onOutline(final Outline o) {
-        return null;
+        return o.getShape().accept(this);
     }
 
     @Override
     public Location onPolygon(final Polygon s) {
-        return null;
+        return onGroup(s);
     }
 }
